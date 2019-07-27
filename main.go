d33b05824725bfa3bb6b3f6d36238fbc6974d5f0
sky0621 <sky14work@gmail.com>
2019-07-27 15:05:11 +0900

    1st

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
 )
 
 func usage() {
@@ -27,12 +28,71 @@ help   ... ヘルプ情報（当内容と同じ）を表示します。
 // ここからメイン処理
 // -------------------------------------------------------------------
 
+var cmdStore = map[string]string{}
+
 func main() {
-	s := bufio.NewScanner(os.Stdin)
-	for s.Scan() {
-		println(s.Text())
-	}
-	if s.Err() != nil {
-		log.Fatal(s.Err())
+	println("Start!")
+	for {
+		s := bufio.NewScanner(os.Stdin)
+		for s.Scan() {
+
+			// アプリ終了判定
+			if s.Text() == "end" {
+				println("End!")
+				os.Exit(-1)
+			}
+
+			// ヘルプ
+			if s.Text() == "help" {
+				usage()
+			}
+
+			if s.Text() == "" {
+				usage()
+				continue
+			}
+
+			// 以降は、引数ありコマンドの処理
+			cmds := strings.Split(s.Text(), " ")
+
+			// 保存
+			if cmds[0] == "save" {
+				if len(cmds) != 3 {
+					usage()
+					continue
+				}
+				cmdStore[cmds[1]] = cmds[2]
+			}
+
+			// 取得
+			if cmds[0] == "get" {
+				if len(cmds) != 2 {
+					usage()
+					continue
+				}
+				println(cmdStore[cmds[1]])
+			}
+
+			// 削除
+			if cmds[0] == "remove" {
+				if len(cmds) != 2 {
+					usage()
+					continue
+				}
+				delete(cmdStore, cmds[1])
+			}
+
+			// 一覧
+			if cmds[0] == "list" {
+				println(`"key","value"`)
+				for k, v := range cmdStore {
+					println("\"" + k + "\",\"" + v + "\"")
+				}
+			}
+
+		}
+		if s.Err() != nil {
+			log.Fatal(s.Err())
+		}
 	}
 }
